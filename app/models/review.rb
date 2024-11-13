474855c2eab5fdd67425395bac2fbfa3ec09c2f8
SniperXDev <abuntrock3@gmail.com>
2024-11-13 03:22:07 -0300

    feat: review model with validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :content, presence: true
+  validates :user_id, uniqueness: { scope: :book_id, message: "can only review a book once" }
 end
